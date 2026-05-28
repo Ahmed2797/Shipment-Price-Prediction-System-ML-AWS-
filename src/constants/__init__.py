238358e42d3141cdad68b464acf22abea07c2785
Ahmed <tanvirahmed754575@gmail.com>
2026-05-28 22:55:41 +0300

    mongdb constant

---
+++ src/constants/__init__.py
@@ -0,0 +1,10 @@
+import os
+import numpy as np
+from datetime import date
+
+Current_Year = date.today().year
+
+# MongoDB
+Data_Base_Name = 'ShipmentDB'
+Collection_name = 'Ship-connection'
+MONGODB_URL_KEY = 'MONGODB_URL' 
