5b81acfc5e6187bd07123adeddb65b1599e8a4a6
Ahmed <tanvirahmed754575@gmail.com>
2026-06-08 14:46:15 +0300

    constant init

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,5 +1,4 @@
 import os
-import numpy as np
 from datetime import date
 
 Current_Year = date.today().year
@@ -8,3 +7,28 @@ Current_Year = date.today().year
 Data_Base_Name = 'ShipmentDB'
 Collection_name = 'Ship-connection'
 MONGODB_URL_KEY = 'MONGODB_URL' 
+
+# Artifacts 
+Artifact = 'artifact'
+Pipeline_dir = 'pipeline'
+final_model = 'final_model'
+
+# Data
+Raw_Data = 'raw.csv'
+Train_Data = 'train.csv'
+Test_Data = 'test.csv' 
+
+# Target_column
+Target_Column = 'Cost'
+
+
+# yaml_file
+COLUMN_YAML_FILE_PATH = os.path.join('config','schema.yaml')
+PARAM_YAML_FILE = os.path.join('yaml_file','model.yaml')
+
+# DataIngestion
+DATA_INGESTION_DIR:str = 'data_ingestion'
+DATA_INGESTION_FEATURE_STORED_DIR:str = Raw_Data #'feature.csv'
+DATA_INGESTION_INGESTED_DIR:str = 'ingested'
+DATA_INGESTION_SPLIT_RATIO:float = 0.2
+Collection_name:str = 'Ship-connection'
