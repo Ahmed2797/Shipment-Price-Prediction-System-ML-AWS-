b982d5c804054eb2e7e182c5d56c0e420c30ffe8
Ahmed <tanvirahmed754575@gmail.com>
2026-06-10 11:31:08 +0300

    constants for validation

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,12 +1,16 @@
 import os
 from datetime import date
+from os import environ
+from dotenv import load_dotenv
+load_dotenv()
 
 Current_Year = date.today().year
 
 # MongoDB
 Data_Base_Name = 'ShipmentDB'
 Collection_name = 'Ship-connection'
-MONGODB_URL_KEY = 'MONGODB_URL' 
+MONGODB_URL_KEY = 'MONGODB_URL'
+# MONGODB_URL_KEY = environ['MONGODB_URL']
 
 # Artifacts 
 Artifact = 'artifact'
@@ -24,7 +28,7 @@ Target_Column = 'Cost'
 
 # yaml_file
 COLUMN_YAML_FILE_PATH = os.path.join('config','schema.yaml')
-PARAM_YAML_FILE = os.path.join('yaml_file','model.yaml')
+PARAM_YAML_FILE = os.path.join('config','model.yaml')
 
 # DataIngestion
 DATA_INGESTION_DIR:str = 'data_ingestion'
@@ -32,3 +36,8 @@ DATA_INGESTION_FEATURE_STORED_DIR:str = Raw_Data #'feature.csv'
 DATA_INGESTION_INGESTED_DIR:str = 'ingested'
 DATA_INGESTION_SPLIT_RATIO:float = 0.2
 Collection_name:str = 'Ship-connection'
+
+# Data_validation 
+DATA_VALIDATION_DIR:str = 'data_validation'
+DATA_VALIDATION_REPORT_DIR:str = 'drift_report'
+DATA_VALIDATION_REPORT_STATUS:str = 'report.yaml' 
