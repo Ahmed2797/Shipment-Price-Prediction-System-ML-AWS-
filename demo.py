d0570ba01ffc9f3e6d0e1d82725f4f4236f8c1bd
Ahmed <tanvirahmed754575@gmail.com>
2026-06-24 11:44:51 +0200

    demo prediction

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
-from src.pipeline.prediction_pipeline import ShipmentPricePredictor
+from src.entity.estimator import ShipmentPricePredictor
 import logging
 import joblib
 
@@ -22,27 +22,48 @@ def pred_model(input_data: dict):
 
         return prediction
 
-        logging.info("Model pipeline loaded successfully.")
     except Exception as e:
         logging.error(f"Model loading failed: {e}")
         raise e
 
 
+# input_data = {
+#   "Artist_Reputation": 3.5,
+#   "Height": 20,
+#   "Width": 15,
+#   "Weight": 50,
+#   "Material": "Marble",
+#   "Price_Of_Sculpture": 5000,
+#   "Base_Shipping_Price": 200,
+#   "International": "N0",
+#   "Express_Shipment": "No",
+#   "Installation_Included": "Yes",
+#   "Transport": "Air",
+#   "Fragile": "Yes",
+#   "Customer_Information": "Premium",
+#   "Remote_Location": "No"
+# }
 input_data = {
-  "Artist_Reputation": 3.5,
-  "Height": 20,
-  "Width": 15,
-  "Weight": 50,
-  "Material": "Marble",
-  "Price_Of_Sculpture": 5000,
-  "Base_Shipping_Price": 200,
-  "International": "N0",
-  "Express_Shipment": "No",
-  "Installation_Included": "Yes",
-  "Transport": "Air",
-  "Fragile": "Yes",
-  "Customer_Information": "Premium",
-  "Remote_Location": "No"
+    "Customer Id": "fffe3900350033003300",
+    "Artist Name": "Billy Jenkins",
+    "Artist Reputation": 0.26,
+    "Height": 17.0,
+    "Width": 6.0,
+    "Weight": 4128.0,
+    "Material": "Brass",
+    "Price Of Sculpture": 13.91,
+    "Base Shipping Price": 16.27,
+    "International": "Yes",
+    "Express Shipment": "Yes",
+    "Installation Included": "No",
+    "Transport": "Airways",
+    "Fragile": "No",
+    "Customer Information": "Working Class",
+    "Remote Location": "No",
+    "Scheduled Date": "06/07/15",
+    "Delivery Date": "06/03/15",
+    "Customer Location": "New Michelle, OH 50777",
+    # "Cost": -283.29
 }
 
 prediction = pred_model(input_data=input_data)
