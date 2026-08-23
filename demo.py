17ecafb8d43a7960e4e1964e74699e2c328c306c
Ahmed <tanvirahmed754575@gmail.com>
2026-08-23 23:52:47 +0200

    demo.py

--- demo.py
+++ demo.py
@@ -9,13 +9,14 @@ def pred_model(input_data: dict):
     Loads the pre-trained model and preprocessing transformer pipelines from 
     disk and instantiates the global `ShipmentPricePredictor`.
     """
-    global pipeline
     try:
-        trained_model = joblib.load("artifact/model_trainer/best_model.pkl")
+        trained_model = joblib.load("aws_model_init/best_model.pkl")
         transform_object = joblib.load("artifact/data_transform/transform_obj/preprocessing.pkl")
+        power_transformer = joblib.load("artifact/data_transform/transform_obj/power_transformer.pkl")
 
         pipeline = ShipmentPricePredictor(
             transform_object=transform_object,
+            power_transformer=power_transformer,
             trained_model=trained_model
         )
         prediction = pipeline.predict_single(input_data)
@@ -27,34 +28,40 @@ def pred_model(input_data: dict):
         raise e
 
 
-# input_data = {
-#   "Artist_Reputation": 3.5,
-#   "Height": 20,
-#   "Width": 15,
-#   "Weight": 50,
-#   "Material": "Marble",
-#   "Price_Of_Sculpture": 5000,
-#   "Base_Shipping_Price": 200,
-#   "International": "N0",
-#   "Express_Shipment": "No",
-#   "Installation_Included": "Yes",
-#   "Transport": "Air",
-#   "Fragile": "Yes",
-#   "Customer_Information": "Premium",
-#   "Remote_Location": "No"
-# }
+shipment_data = {
+    "Customer Id": "fffe3800330031003900",
+    "Artist Name": "Jean Bryant",
+    "Artist Reputation": 0.28,
+    "Height": 3.0,
+    "Width": 3.0,
+    "Weight": 61.0,
+    "Material": "Brass",
+    "Price Of Sculpture": 6.83,
+    "Base Shipping Price": 15.0,
+    "International": "No",
+    "Express Shipment": "No",
+    "Installation Included": "No",
+    "Transport": "Roadways",
+    "Fragile": "No",
+    "Customer Information": "Working Class",
+    "Remote Location": "No",
+    "Scheduled Date": "03/06/17",
+    "Delivery Date": "03/05/17",
+    "Customer Location": "New Michaelport, WY 12072"
+    ## "Cost": -159.96
+}
 input_data = {
     "Customer Id": "fffe3900350033003300",
     "Artist Name": "Billy Jenkins",
-    "Artist Reputation": 0.26,
-    "Height": 17.0,
-    "Width": 6.0,
-    "Weight": 4128.0,
+    "Artist Reputation": 0.28,
+    "Height": 3.0,
+    "Width": 3.0,
+    "Weight": 61.0,
     "Material": "Brass",
-    "Price Of Sculpture": 13.91,
-    "Base Shipping Price": 16.27,
-    "International": "Yes",
-    "Express Shipment": "Yes",
+    "Price Of Sculpture": 6.91,
+    "Base Shipping Price": 15.00,
+    "International": "No",
+    "Express Shipment": "No",
     "Installation Included": "No",
     "Transport": "Airways",
     "Fragile": "No",
@@ -66,5 +73,28 @@ input_data = {
     # "Cost": -283.29
 }
 
-prediction = pred_model(input_data=input_data)
+shipment_data2 = {
+    "Customer Id": "fffe3800360033003700",
+    "Artist Name": "David Hawes",
+    "Artist Reputation": 0.64,
+    "Height": 17.0,
+    "Width": 9.0,
+    "Weight": 7264.0,
+    "Material": "Brass",
+    "Price Of Sculpture": 8.26,
+    "Base Shipping Price": 90.67,
+    "International": "No",
+    "Express Shipment": "Yes",
+    "Installation Included": "No",
+    "Transport": "Roadways",
+    "Fragile": "No",
+    "Customer Information": "Working Class",
+    "Remote Location": "No",
+    "Scheduled Date": "06/05/16",
+    "Delivery Date": "06/02/16",
+    "Customer Location": "South Matthew, WV 76033"
+##   "Cost": -1536.66
+}
+
+prediction = pred_model(input_data=shipment_data2)
 print(prediction)
